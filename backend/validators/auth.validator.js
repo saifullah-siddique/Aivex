cd81470440c9c4ee9834bcaa6b5b6d4a417740d9
saifullah-siddique <saifullahshasiddiqui@gmail.com>
2026-02-11 21:25:57 +0530

    create a validator for schema check

---
+++ backend/validators/auth.validator.js
@@ -0,0 +1,47 @@
+import { body } from "express-validator";
+
+export const registerValidator = [
+    body("name")
+        .trim()
+        .notEmpty()
+        .withMessage("Name is required")
+        .bail()
+        .isLength({ min: 3 })
+        .withMessage("Name must be at least 3 characters long")
+        .bail()
+        .matches(/^[A-Za-z ]+$/)
+        .withMessage("Name must contain only letters and spaces"),
+
+    body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("Username is required")
+        .bail()
+        .isLength({ min: 3, max: 20 })
+        .withMessage("Username must be between 3 and 20 characters")
+        .bail()
+        .toLowerCase()
+        .matches(/^[a-z0-9_]+$/)
+        .withMessage("Invalid Username"),
+
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .bail()
+        .isEmail()
+        .withMessage("Invalid email format"),
+
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+        .bail()
+        .isLength({ min: 8 })
+        .withMessage("Password must be at least 8 characters long"),
+];
+
+
+export const loginValidator = [
+    body("email").isEmail().withMessage("Email must be valid address"),
+    body("password").isLength({ min: 8 }).withMessage("Password must be at least 8 charecters long"),
+]
