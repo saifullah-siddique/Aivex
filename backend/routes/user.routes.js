3983be5438176c0a88fe1447bee8d191949fec25
saifullah-siddique <saifullahshasiddiqui@gmail.com>
2026-02-11 21:37:30 +0530

    removed validation code

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,27 +1,22 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller.js";
-import { body } from "express-validator";
 import * as authMiddleware from "../middleware/auth.middleware.js";
+import * as authvalidator from "../validators/auth.validator.js";
+import { validate } from "../middleware/validate.middleware.js";
 
 const router = Router();
 
 
-router.post("/register",
-    body("email").isEmail().withMessage("Email must be a valid address"),
-    body("password").isLength({ min: 3 }).withMessage("Password must be at least 3 charecters long"),
-    userController.createUserController);
+router.post("/register", authvalidator.registerValidator, validate, userController.createUserController);
 
-router.post("/login",
-    body("email").isEmail().withMessage("EMail must be valid address"),
-    body("password").isLength({ min: 3 }).withMessage("Password must be at least 3 charecters long"),
-    userController.loginController);
+router.post("/login", authvalidator.loginValidator, validate, userController.loginController);
 
 router.get("/profile", authMiddleware.authUser, userController.profileController);
 
 router.post("/logout", userController.logoutController);
 
-// router.get("/all", authMiddleware.authUser, userController.getAllUsersController);  //will be used in dev/admin panel to moniter whole site
-
 router.get("/search", authMiddleware.authUser, userController.searchUserController)
+
+// router.get("/all", authMiddleware.authUser, userController.getAllUsersController);  //will be used in dev/admin panel to moniter whole site
 
 export default router;
